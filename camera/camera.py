2409603c4e581f7c742d0443379557c5201ba75d
Hennzau <dev@enzo-le-van.fr>
2024-05-03 19:40:24 +0200

    camera node

--- camera/camera.py
+++ camera/camera.py
@@ -1,15 +1,21 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 from dora import Node
+import cv2
 
 node = Node()
 
-event = node.next()
-if event["type"] == "INPUT":
-    print(
-        f"""Node received:
-    id: {event["id"]},
-    value: {event["value"]},
-    metadata: {event["metadata"]}"""
-    )
+video_capture = cv2.VideoCapture(3)
+video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
+for event in node:
+    if event["type"] == "INPUT":
+        ret, frame = video_capture.read()
+        if ret:
+            frame = cv2.resize(frame, (320, 240))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+
+            node.send_output(
+                "image",
+                cv2.imencode(".jpg", frame)[1].tobytes(),
+                event["metadata"]
+            )
